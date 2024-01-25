703ca05ceb8d03f035d53e806c2e62822edf9b83
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2024-01-25 21:59:07 +0500

    arrays

---
+++ arrays_objects/src/main.ts
@@ -0,0 +1,39 @@
+let stringArr = ['one', 'hey', 'Dave']
+
+let guitars = ['Strat', 'Les Paul', 5150]
+
+let mixedData = ['EVH', '1984', true]
+
+stringArr[0] = 'John'
+// stringArr[0] = 1 
+// error
+
+stringArr.push('hey')
+// stringArr.push(1)
+// error
+
+guitars.unshift('Jim') // valid
+guitars.unshift('1000') // valid
+// guitars.unshift(true) // invalid
+
+// stringArr = guitars
+// error
+guitars = stringArr
+
+let test = []
+let bands: string[] = []
+bands.push('Van Halen')
+
+// Tuple
+let myTuple: [string, number, boolean] = ['Dave', 42, true]
+
+// myTuple[3] = "Anything"
+// error
+
+let mixed = ['John', 1, false]
+
+mixed = myTuple
+
+// myTuple = mixed
+// error
+
